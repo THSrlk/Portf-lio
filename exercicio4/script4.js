18906173d3872c2503fe0176c9eda02353fa64c0
Matheus <matheus.cunha.freitas@escola.pr.gov.br>
2025-05-26 11:26:07 -0300

    Add files via upload

--- exercicio4/script4.js
+++ exercicio4/script4.js
@@ -2,17 +2,15 @@ function media() {
     let nota1 = Number(window.prompt('Digite a primeira nota'));
     let nota2 = Number(window.prompt('Digite a segunda nota'));
     let nota3 = Number(window.prompt('Digite a terceira nota'));
-   
+
     let media = (nota1 + nota2 + nota3) / 3;
 
     let resultado = document.querySelector('#resultado');
-    
-    if (media >= 6) {
-        resultado.innerHTML = '<p>O resultado é ' + media.toFixed(2) + '. Aluno aprovado!</p>';
-    } else {
-        let falta = (6 - media).toFixed(2);
-        resultado.innerHTML = '<p>O resultado é ' + media.toFixed(2) + '. Aluno reprovado! Faltam ' + falta + ' pontos para passar. Vai fazer o se liga!</p>';
-    }
 
-    console.log(resultado);
+    if (media >= 6) {
+        resultado.innerHTML = "O aluno foi aprovado! Média: " + media;
+    } else {
+        let dif = 6 - media;
+        resultado.innerHTML = "Aluno reprovado! Média: " + media + ". Faltaram " + dif + " ponto(s) para passar. Vai fazer o Se Liga!";
+    }
 }
